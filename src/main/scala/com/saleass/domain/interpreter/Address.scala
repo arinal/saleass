15e0da76691ca1695edf49ea36a8fcf6b67682f6
dicky.arinal <dicky.arinal@gmail.com>
2016-11-09 20:34:54 +0700

    Move address to another file

---
+++ src/main/scala/com/saleass/domain/interpreter/Address.scala
@@ -0,0 +1,32 @@
+package com.saleass.domain.interpreter
+
+import com.lamedh.common.Lens
+
+case class Address(no: String, street: String, city: String, state: String, zip: String)
+
+trait AddressLenses {
+  val noLens = Lens[Address, String](
+    get = _.no,
+    set = (e, no) => e.copy(no = no)
+  )
+
+  val streetLens = Lens[Address, String](
+    get = _.street,
+    set = (e, street) => e.copy(street = street)
+  )
+
+  val cityLens = Lens[Address, String](
+    get = _.city,
+    set = (e, city) => e.copy(city = city)
+  )
+
+  val stateLens = Lens[Address, String](
+    get = _.state,
+    set = (e, state) => e.copy(state = state)
+  )
+
+  val zipLens = Lens[Address, String](
+    get = _.zip,
+    set = (e, zip) => e.copy(zip = zip)
+  )
+}
--- src/main/scala/com/saleass/domain/interpreter/Employee.scala
+++ src/main/scala/com/saleass/domain/interpreter/Employee.scala
@@ -3,34 +3,6 @@ package com.saleass.domain.interpreter
 import com.lamedh.common._
 
 case class Employee(id: Long, code: String, name: String, address: Address = Address("", "", "", "", ""))
-case class Address(no: String, street: String, city: String, state: String, zip: String)
-
-trait AddressLenses {
-  val noLens = Lens[Address, String](
-    get = _.no,
-    set = (e, no) => e.copy(no = no)
-  )
-
-  val streetLens = Lens[Address, String](
-    get = _.street,
-    set = (e, street) => e.copy(street = street)
-  )
-
-  val cityLens = Lens[Address, String](
-    get = _.city,
-    set = (e, city) => e.copy(city = city)
-  )
-
-  val stateLens = Lens[Address, String](
-    get = _.state,
-    set = (e, state) => e.copy(state = state)
-  )
-
-  val zipLens = Lens[Address, String](
-    get = _.zip,
-    set = (e, zip) => e.copy(zip = zip)
-  )
-}
 
 trait EmployeeLenses {
   val codeLens = Lens[Employee, String](
